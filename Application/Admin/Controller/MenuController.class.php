c62644b7172d94847d56182a005c3c7542754318
samnew <samnew@126.com>
2016-08-28 23:46:18 +0800

    add MenuController.class.php

---
+++ Application/Admin/Controller/MenuController.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Admin\Controller;
+
+class MenuController extends CommonController{
+	public function index(){
+		$this->display();
+	}
+
+}
---
+++ Public/js/admin/common.js
@@ -0,0 +1,21 @@
+/**
+ * 添加按钮操作
+ */
+
+ $("#button-add").click(function(){
+ 	var url = SCOPE.add_url;
+ 	window.location.href=url;
+ });
+
+ /**
+  * 提交form表单操作
+  */
+ 
+ $("#cms-button-submit").click(function(){
+ 	var data =$("#cms-form").serializeArray();
+ 	postData = {};
+ 	$(data).each(function(i){
+ 		postData[this.name]=this.value;
+ 	});
+ 	console.log(postData);
+ });
