3ae67bde85deb2de706d483ea71d7d8aa2577883
samnew <samnew@126.com>
2016-09-07 10:10:54 +0800

    add position content list module

--- Application/Admin/Controller/PositioncontentController.class.php
+++ Application/Admin/Controller/PositioncontentController.class.php
@@ -5,7 +5,17 @@ use Think\Controller;
 class PositioncontentController extends CommonController{
 	public function index(){
 		$positions = D('Position')->getNormalPositions();
+		//获取推荐位里面的内容
+		$data['status']= array('neq',-1);
+		if($_GET['title']){
+			$data['title'] = trim($_GET['title']);
+			$this->assign('title',$data['title']);
+		}
+		$data['position_id']=$_GET['position_id']?intval($_GET['position_id']):$positions[0]['id'];
+		$contents = D("PositionContent")->select($data);
+		$this->assign('contents',$contents);
 		$this->assign("positions",$positions);
+		$this->assign("positionId",$data['position_id']);
 		$this->display();
 	}
 }
--- Application/Common/Model/PositionContentModel.class.php
+++ Application/Common/Model/PositionContentModel.class.php
@@ -11,4 +11,16 @@ class PositionContentModel extends Model{
 
 		return $this->_db->data($data)->add();
 	}
+	public function select($data=array(),$limit=0){
+		if($data['title']){
+			$data['title'] =array('like','%'.$data['title'].'%');	
+		}
+		$this->_db->where($data)->order('listorder desc,id desc');
+		if($limit){
+			$this->_db->limit($limit);
+		}
+		$list = $this->_db->select();
+		return $list;
+	}
+
 }
